539c11777d4a2c56f82a00acc90bcac3fddf4eab
Dimitri Buhon <bdimitri75@gmail.com>
2018-01-09 18:57:19 +0100

    move background on mouse event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { AppService } from './services/app.service';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  @ViewChild('root') root: any;
+
+  moveBackground(event: MouseEvent) {
+    this.root.nativeElement.style.backgroundPositionX = (event.x / 10).toString() + 'px';
+    this.root.nativeElement.style.backgroundPositionY = (event.y / 10).toString() + 'px';
+  }
 }
