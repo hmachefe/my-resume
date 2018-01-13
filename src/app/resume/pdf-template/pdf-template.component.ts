ebc7056e03483547b16dda3fe6bab13ee20db20e
Dimitri75 <bdimitri75@gmail.com>
2018-01-13 12:38:21 +0100

    update pdf resume

--- src/app/resume/pdf-template/pdf-template.component.ts
+++ src/app/resume/pdf-template/pdf-template.component.ts
@@ -13,6 +13,25 @@ export class PdfTemplateComponent implements OnInit {
   constructor(private appService: AppService) { }
 
   ngOnInit() {
-    this.appService.getResume().subscribe(response => this.resume = response.json());
+    this.appService.getResume().subscribe(response => {
+      const resume = response.json();
+      this.reduceResume(resume);
+      this.resume = resume;
+    });
+  }
+
+  private reduceResume(resume: Resume) {
+    this.reduceCareer(resume);
+  }
+
+  private reduceCareer(resume: Resume) {
+    resume.career = resume.career.filter(career => career.description && career.description.length > 0);
+    resume.career.length = 3;
+
+    resume.career.forEach(job => {
+      job.quote = null;
+      job.location = null;
+      job.logo = null;
+    });
   }
 }
